cb2570284c3fe9ef7150a80bde3cbc440f798635
Sam Natale <sam.natale@agsenze.com>
2024-10-16 02:04:11 +0000

    updated install_script

--- scripts/.config/scripts/install_script.sh
+++ scripts/.config/scripts/install_script.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-# update and upgrade packages
+echo "Updating and upgrading packages"
 sudo apt -y update
 sudo apt -y upgrade
 
-# install packages
+echo "Installing packages..."
 sudo apt -y install i3 kitty rofi thunar i3blocks wireless-tools picom stow nitrogen neovim zsh ripgrep | exit
 
-# set zsh as default not bash
+echo "Setting default shell to zsh..."
 chsh -s $(which zsh)
 
-# install fzf from source
+echo "Installing fzf from source..."
 mkdir -p $HOME/.fzf
 cd $HOME/.fzf
 git clone git@github.com:junegunn/fzf.git
 mv $HOME/.fzf/fzf/* $HOME/.fzf
 chmod +x install
 ./install
+
+echo "Setting nvim as default git editor..."
+git config --global core.editor "nvim"
+
+echo "Cloning dotfiles"
+cd $HOME
+git clone git@github.com:samnatale3/dotfiles.git
+
+echo "Stowing dotfiles"
+stow dunst i3 i3blocks kitty nvim picom rofi scripts zsh
