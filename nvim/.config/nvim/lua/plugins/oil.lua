a0eeda04bd416ab4955ba26637e0bff807454ac1
Sam Natale <samnatale3@gmail.com>
2024-11-04 11:14:12 +0000

    vim api make oil transparent

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,15 +1,16 @@
 return {
-  {
-  "stevearc/oil.nvim",
-  opts = {
-    view_options = {
-      show_hidden = true,
-    },
-  },
-  config = function(_, opts)
-    local oil = require("oil")
-    oil.setup(opts)
-    vim.keymap.set("n", "-", oil.toggle_float, {})
-  end,
-  },
+	{
+		"stevearc/oil.nvim",
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
+		config = function(_, opts)
+			local oil = require("oil")
+			oil.setup(opts)
+			vim.keymap.set("n", "-", oil.toggle_float, {})
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		end,
+	},
 }
