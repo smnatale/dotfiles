b1dceefa0b8140253c9ff0d90a29403d870f9e65
Sam Natale <samnatale3@gmail.com>
2025-03-07 22:36:47 +0000

    lualine fixes, wezterm add swapping windows

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -2,13 +2,7 @@ return {
   {
     "windwp/nvim-ts-autotag",
     config = function()
-      require('nvim-ts-autotag').setup({
-        opts = {
-          enable_close = true,
-          enable_rename = true,
-          enable_close_on_slash = false
-        },
-      })
+      require('nvim-ts-autotag').setup()
     end
   },
   {
@@ -17,5 +11,38 @@ return {
     opts = {
       enable_autocmd = false,
     },
+  },
+  {
+    "j-hui/fidget.nvim",
+    opts = {},
+  },
+  {
+    'b0o/incline.nvim',
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "folke/tokyonight.nvim",
+    },
+    config = function()
+      local devicons = require 'nvim-web-devicons'
+      require('incline').setup {
+        window = {
+          padding = 0,
+          margin = { horizontal = 0 },
+        },
+        hide = { cursorline = true },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+          return {
+            ' ',
+            { (ft_icon or '') .. ' ', guifg = ft_color,                          guibg = 'none' },
+            ' ',
+            { filename,               gui = modified and 'bold,italic' or 'bold' },
+            ' ',
+          }
+        end,
+      }
+    end,
   },
 }
