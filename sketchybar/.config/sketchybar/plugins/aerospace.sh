417c340426bbf1f67efd33c7384000d364cfa596
Sam Natale <samnatale3@gmail.com>
2025-11-14 10:47:37 +0000

    rm bg of selected workspace

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on background.color=0xff090b10 label.color=0xffe0def4
+    sketchybar --set $NAME background.drawing=off label.color=0xffe0def4
 else
     sketchybar --set $NAME background.drawing=off label.color=0xff6e6a86
 fi
