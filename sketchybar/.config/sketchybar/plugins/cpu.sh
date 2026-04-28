92de872f0a40eea14a0e277e75587969674ead70
smnatale <samnatale3@gmail.com>
2026-04-28 19:49:49 +0100

    minor tweaks

--- sketchybar/.config/sketchybar/plugins/cpu.sh
+++ sketchybar/.config/sketchybar/plugins/cpu.sh
@@ -8,8 +8,16 @@ case "$SENDER" in
     sketchybar --set cpu popup.drawing=off
     ;;
   *)
-    LOAD=$(ps -A -o %cpu | awk '{s+=$1} END {print s/4}' | cut -d. -f1)
-    [ -z "$LOAD" ] && LOAD=0
-    sketchybar --set "$NAME" icon="󰆼" label="${LOAD}%"
+    TOPPROC=$(ps -Arco pid,%cpu,comm | head -2 | tail -1)
+    CPU_PCT=$(echo "$TOPPROC" | awk '{print $2}' | cut -d. -f1)
+    PROC_NAME=$(echo "$TOPPROC" | awk '{print $3}' | cut -c1-14)
+
+    TOTAL=$(ps -A -o %cpu | awk '{s+=$1} END {printf "%.0f", s/4}')
+    [ -z "$TOTAL" ] && TOTAL=0
+
+    GRAPH_VAL=$(echo "$TOTAL" | awk '{v=$1/100; if(v>1) v=1; printf "%.2f", v}')
+
+    sketchybar --push cpu "$GRAPH_VAL" \
+               --set cpu.top label="${CPU_PCT} ${PROC_NAME}"
     ;;
 esac
