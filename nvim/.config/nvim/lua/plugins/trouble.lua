451e66a04417d3b19353678c5bbca3f6f789f228
smnatale <samnatale3@gmail.com>
2026-07-13 15:53:59 +0100

    nvim: add todo-comments

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,13 +1,16 @@
 -- plugins
 vim.pack.add({
 	"https://github.com/folke/trouble.nvim",
+	"https://github.com/folke/todo-comments.nvim",
 })
 
 -- options
 require("trouble").setup()
+require("todo-comments").setup()
 
 -- keymaps
 vim.keymap.set("n", "<leader>tr", "<cmd>Trouble diagnostics toggle<cr>", { silent = true })
+vim.keymap.set("n", "<leader>td", "<cmd>Trouble todo toggle<cr>", { silent = true })
 
 -- auto open trouble quickfix list
 vim.api.nvim_create_autocmd("BufRead", {
