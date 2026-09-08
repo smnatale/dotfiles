7095cc2a37fec22a1c3e3d128f136872780e30d4
smnatale <samnatale3@gmail.com>
2026-09-08 21:37:04 +0100

    improve sketchybar

--- sketchybar/.config/sketchybar/plugins/youtube.sh
+++ sketchybar/.config/sketchybar/plugins/youtube.sh
@@ -21,10 +21,26 @@ case "$COUNT" in
   ''|*[!0-9.,KkMmBb]*) COUNT="" ;;
 esac
 
+format_count() {
+  printf '%s\n' "$1" | perl -ne '
+    chomp;
+    if (/^([0-9]+(?:\.[0-9]+)?)([KkMmBb])?$/) {
+      my ($number, $suffix) = ($1, $2 // "");
+      my $multiplier = $suffix =~ /[Kk]/ ? 1000 :
+        $suffix =~ /[Mm]/ ? 1000000 :
+        $suffix =~ /[Bb]/ ? 1000000000 : 1;
+      my $whole = int($number * $multiplier + 0.5);
+      $whole =~ s/(?<=\d)(?=(\d{3})+$)/,/g;
+      print $whole;
+    }
+  '
+}
+
 if [ -n "$COUNT" ]; then
   printf '%s\n' "$COUNT" > "$CACHE_FILE"
 else
   COUNT=$(cat "$CACHE_FILE" 2>/dev/null || true)
 fi
 
-sketchybar --set "$NAME" label="${COUNT:----}"
+DISPLAY_COUNT=$(format_count "$COUNT")
+sketchybar --set "$NAME" label="${DISPLAY_COUNT:----}"
