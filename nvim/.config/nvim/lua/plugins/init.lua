a46a4039e15bde86f936606496c89fc50a9e3cfc
smnatale <66391194+smnatale@users.noreply.github.com>
2026-09-22 13:39:45 +0100

    explain my plugins

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,13 +1,23 @@
 vim.pack.add({
+	-- colorscheme
 	{ src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
+	-- lsp configurations
 	"https://github.com/neovim/nvim-lspconfig",
+	-- treesitter
 	"https://github.com/nvim-treesitter/nvim-treesitter",
+	-- snippets to load
 	"https://github.com/rafamadriz/friendly-snippets",
+	-- formatting on save for linters/formatters
 	"https://github.com/stevearc/conform.nvim",
+	-- lots of qol things and core functionality
 	"https://github.com/nvim-mini/mini.nvim",
+	-- the goat file manager
 	"https://github.com/stevearc/oil.nvim",
+	-- add gitstatus to the goat file manager
 	"https://github.com/refractalize/oil-git-status.nvim",
+	-- auto load into jsonls and yamlls completions from SchemaStore
 	"https://github.com/b0o/SchemaStore.nvim",
+	-- diff viewer inspired by zed diff (multibuffer)
 	"https://github.com/martindur/zdiff.nvim",
 })
 
