74a753dde13589dd8e5f2e4adf1b7f2c50f24945
smnatale <66391194+smnatale@users.noreply.github.com>
2026-09-22 13:03:52 +0100

    cleanup missing vim.pack

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,3 @@
-vim.pack.add({
-	"https://github.com/stevearc/oil.nvim",
-	"https://github.com/refractalize/oil-git-status.nvim",
-})
-
 require("oil").setup({
 	win_options = {
 		signcolumn = "yes:2",
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,3 @@
-vim.pack.add({
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-})
-
 local languages = {
 	"bash",
 	"css",
